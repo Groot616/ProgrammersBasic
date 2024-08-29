a0fb74b26739be464e0263d41e552887fa832196
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-08-30 02:53:47 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -1,27 +1,28 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
 vector<int> solution(vector<int> emergency) {
-    int size = emergency.size();
-    vector<int> answer(size);
-    int cnt = 1;
-
-    while (cnt <= size)
-    {
-        int max = *max_element(emergency.begin(), emergency.end());
-        for (int i = 0; i < size; ++i)
-        {
-            if (emergency[i] == max)
-            {
-                answer[i] = cnt;
-                emergency[i] = 0;
-                ++cnt;
-            }
-        }
-    }
-
+    vector<int> answer;
+    
+    auto amt = map<int, int>();
+    // 반복을 통해 map에 키 추가, 이때 중복은 제외되며, 오름차순으로 자동 정렬
+    for(auto& e : emergency)
+        amt[e];
+    
+    // 순위를 입력하기 위한 사이즈
+    auto size = amt.size();
+    
+    // 반복문을 통해 오름차순된 map의 아래부터 --사이즈하여 순위 등록
+    for(auto& e : amt)
+        e.second = size--;
+    
+    // emergency를 순위로 업데이트
+    for(auto& e : emergency)
+        e = amt[e];
+    
+    answer = emergency;
     return answer;
 }
