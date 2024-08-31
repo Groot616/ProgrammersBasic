0c03db7897e711e0c71d6e64d4553ac9e5c88ed3
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-08-31 20:38:48 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -1,54 +1,27 @@
 #include <string>
 #include <vector>
-#include <set>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 int solution(vector<int> array) {
     int answer = 0;
-
-    // 주어진 수가 하나일 경우 무조건 그 수가 최빈값
-    if(array.size() == 1)
+    
+    unordered_map<int, int> arr;
+    for(const auto& e : array)
+        arr[e]++;
+    
+    int maxVal = 0;
+    for(const auto& e : arr)
     {
-        answer = array[0];
-        return answer;
+        if(e.second > maxVal)
+        {
+            maxVal = e.second;
+            answer = e.first;
+        }
+        else if(maxVal == e.second)
+            answer = -1;
     }
     
-    // set으로 중복값 없이 정렬 및 값 벡터로 치환
-    set<int> tmp;
-    for (const auto& e : array)
-        tmp.insert(e);
-
-    vector<int> element(tmp.begin(), tmp.end());
-    // 순환하며 확인
-    vector<int> count;
-    for (int i = 0; i < element.size(); ++i)
-    {
-        int cnt = 0;
-        for (int j = 0; j < array.size(); ++j)
-        {
-            if (element[i] == array[j])
-                cnt += 1;
-        }
-        count.push_back(cnt);
-    }
-    // 최대값 위치 구하기
-    auto it = *max_element(count.begin(), count.end());
-    for (int i = 0; i < count.size(); ++i)
-    {
-        if (it == count[i])
-        {
-            for(int j=i+1; j<count.size();++j)
-            {
-                if(it == count[j])
-                {
-                    answer = -1;
-                    return answer;
-                }
-            }
-            answer = element[i];
-            return answer;
-        }
-    }
+    return answer;
 }
