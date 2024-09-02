0e4b0ebe1359f7d0bef22eafd396732f8da2dd8f
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-09-02 09:53:12 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(int num, int total) {
+    vector<int> answer;
+    
+    int start = (total/num) - ((num-1)/2);
+    
+    for(int i=0; i<num; ++i)
+    {
+        answer.push_back(start);
+        ++start;
+    }
+    
+    return answer;
+}
