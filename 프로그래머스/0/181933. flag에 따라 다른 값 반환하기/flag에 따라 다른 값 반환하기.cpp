49e9a18a1aa8983ba4d2313ccbe1ba62eaa3005c
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-09-04 01:19:08 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, bool flag) {
+    int answer = 0;
+    
+    flag == true ? answer = a + b : answer = a - b;
+    
+    return answer;
+}
