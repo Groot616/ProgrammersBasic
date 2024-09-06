11272c0b10c67280353651c63ef671ffc38a5339
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-09-06 21:47:38 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.cpp
@@ -1,38 +1,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
+bool studentcomp(pair<int, int> stu1, pair<int, int> stu2)
+{
+    return stu1.second < stu2.second;
+}
+
 int solution(vector<int> rank, vector<bool> attendance) {
     int answer = 0;
-
-    int cnt = 0;
-    for (const auto& e : attendance)
+    
+    vector<pair<int, int>> student;
+    for(int i=0; i<attendance.size(); ++i)
     {
-        if (e == true)
-            ++cnt;
+        if(attendance[i]) student.push_back({i, rank[i]});
     }
-
-    vector<vector<int>> rankAttendance(cnt, vector<int>(2));
-    //cout << rankAttendance.size() << ", " << rankAttendance[3].size() << endl;
-    int arrNum = 0;
-    for (int i = 0; i< attendance.size(); ++i/*const auto& e : attendance*/)
-    {
-        if (attendance[i] == true/*e == true*/)
-        {
-            for (int j = i; j < attendance.size(); ++j)
-            {
-                rankAttendance[arrNum][0] = rank[j];
-                rankAttendance[arrNum][1] = j;
-                ++arrNum;
-                break;
-            }
-        }  
-    }
-    sort(rankAttendance.begin(), rankAttendance.end());
-    answer = rankAttendance[0][1] * 10000 + rankAttendance[1][1] * 100 + rankAttendance[2][1];
-
+    sort(student.begin(), student.end(), studentcomp);
+    answer = 10000 * student[0].first + 100 * student[1].first + student[2].first;
+    
     return answer;
 }
