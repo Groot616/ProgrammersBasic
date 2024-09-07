701c426e9f9d3d11ebada1d705844c155fab5690
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-09-07 20:12:58 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
@@ -5,54 +5,30 @@
 using namespace std;
 
 int solution(vector<string> spell, vector<string> dic) {
-    int answer = 0;
+    int answer = 2;
     
     // bool 타입 벡터 하나 생성하고 사이즈는 spell의 사이즈와 동일하게, 기본 false로 할당
     // 1. 루프를 통해 dic의 모든 원소에 접근
     // 2. 해당 원소의 알파벳중에 spell과 동일한게 있다면 각 idx true로 변경
     // bool 타입 벡터 전부 true일 경우 result = 1
-    vector<int> cnt(spell.size(), 0);
-    for(int i=0; i<dic.size(); ++i)
+    for(const auto& d : dic)
     {
-        for(int j=0; j<dic[i].size(); ++j)
+        bool alpha = true;
+        for(const auto& s : spell)
         {
-            for(int k=0; k<spell.size(); ++k)
+            if(d.find(s) == string::npos)
             {
-                if(dic[i][j] == spell[k][0])
-                {
-                    // s 있어서 cnt 올라가고 o 두번 카운트 올라가서 3으로 인식되는 문제
-                    ++cnt[k];
-                    cout << "i : " << i << ", j : " << j << ", k : " << k << endl;
-                    cout << "dic[i][j] : " << dic[i][j] << ", spell[k] : " << spell[k] << endl;
-                    cout << "-------------------------" << endl;
-                }
-            } 
-        }
-        cout << "onc cycle overed, cnt : ";
-        for(const auto& e : cnt)
-            cout << e << " ";
-        cout << endl;
-        cout << "=========================" << endl;
-        int check = 0;
-        for(auto& e : cnt)
-        {
-            if(e == 1)
-            {
-                check += 1;;
+                alpha = false;
+                break;
             }
         }
-        if(check == spell.size())
+        
+        if(alpha)
         {
             answer = 1;
-            return answer;
-        }
-        else
-        {
-            for(int u=0; u<spell.size(); ++u)
-                cnt[u] = 0;
+            break;
         }
     }
-    answer = 2;
     
     return answer;
 }
