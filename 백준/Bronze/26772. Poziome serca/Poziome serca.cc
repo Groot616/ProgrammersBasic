a4b622a4701b46938d838f1ed6e24e3bc4c3392f
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-09-24 02:46:34 +0900

    [Bronze IV] Title: Poziome serca, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cout << " @@@   @@@  ";
+	cout << '\n';
+	for(int i=0; i<n; ++i)
+		cout << "@   @ @   @ ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << "@    @    @ ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << "@         @ ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << " @       @  ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << "  @     @   ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << "   @   @    ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << "    @ @     ";
+	cout << '\n';
+	for (int i = 0; i < n; ++i)
+		cout << "     @      ";
+	cout << '\n';
+
+	return 0;
+}
