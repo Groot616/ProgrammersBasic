3e5c91a2cf0fe58a9b47959837898549358b6850
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-09-25 01:55:57 +0900

    [Bronze IV] Title: Atrium, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	long long n;
+
+	cin >> n;
+	cout << fixed;
+	cout.precision(8);
+	cout << 4 * sqrt(n) << '\n';
+
+	return 0;
+}
