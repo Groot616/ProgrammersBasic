81113edaf9a5b0bdf64378b816f2e89b17ba8d70
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-10-05 15:27:21 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+
+	cin >> n;
+	for (int j = 0; j < n; ++j)
+	{
+		for (int i = 0; i < n; ++i)
+			cout << "@";
+		for (int i = 0; i < n; ++i)
+			cout << "@@@@";
+		cout << '\n';
+	}
+	for (int j = 0; j < n; ++j)
+	{
+		for (int i = 0; i < n; ++i)
+			cout << "@";
+		cout << '\n';
+		for (int i = 0; i < n; ++i)
+			cout << "@";
+		cout << '\n';
+		for (int i = 0; i < n; ++i)
+			cout << "@";
+		cout << '\n';
+	}
+	for (int j = 0; j < n; ++j)
+	{
+		for (int i = 0; i < n; ++i)
+			cout << "@";
+		for (int i = 0; i < n; ++i)
+			cout << "@@@@";
+		cout << '\n';
+	}
+
+	return 0;
+}
