2bcc4dd3f98f3b517262c528a121f55a613c36bd
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-10-07 19:32:27 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+float getScore(float n, char ch)
+{
+	if (ch == '+') return n + 0.3;
+	else if (ch == '0') return n;
+	else if (ch == '-') return n - 0.3;
+}
+
+float getGrade(string grade)
+{
+	if (grade == "F")
+		return 0.f;
+
+	vector<char> alpha = { 'A', 'B', 'C', 'D', 'F' };
+	for (int i = 0; i < alpha.size(); ++i)
+	{
+		if (grade[0] == alpha[i])
+			return getScore(float(alpha.size() - 1 - i), grade[1]);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	float average = 0;
+	float totalCredit = 0;
+
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; ++i)
+	{
+		string str;
+
+		getline(cin, str);
+		stringstream ss(str);
+		string subject, credit, grade;
+		ss >> subject >> credit >> grade;
+		average += stof(credit) * getGrade(grade);
+		totalCredit += stof(credit);
+	}
+	float answer = round(average / totalCredit * 100.f) / 100.f;
+	
+	/*string strAnswer = to_string(answer);
+	stringstream ss(strAnswer);
+	string word = "";
+	vector<string> tmp;
+	while (getline(ss, word, '.'))
+		tmp.push_back(word);
+	if (tmp[1].length() == 1)
+	{
+		tmp[1] += "0";
+		cout << tmp[0] << "." << tmp[1] << '\n';
+	}*/
+	cout << fixed << setprecision(2) << answer << '\n';
+
+	return 0;
+}
