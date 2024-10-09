365889002941d969a822bacb282f46e2982f5517
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-10-10 05:24:41 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int t;
+	long long a, b;
+
+	cin >> t;
+	for (int i = 0; i < t; ++i)
+	{
+		cin >> a >> b;
+		cout << "yes" << '\n';
+	}
+
+	return 0;
+}
