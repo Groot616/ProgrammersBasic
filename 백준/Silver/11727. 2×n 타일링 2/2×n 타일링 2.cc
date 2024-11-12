cd6c6e5057ae27c79bf134db9a866f1f648938df
Groot616 <97540029+Groot616@users.noreply.github.com>
2024-11-12 10:12:47 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<long long> tile;
+
+int dp(long long n)
+{
+    if (n == 0) return 0;
+    else
+    {
+        if (tile[n] != 0)
+            return tile[n];
+        else
+        {
+            tile[n] = (dp(n - 1) + 2 * dp(n - 2)) % 10007;
+            return tile[n];
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long long n;
+
+    cin >> n;
+    tile.resize(n + 1);
+    tile[0] = 0;
+    tile[1] = 1;
+    tile[2] = 3;
+    cout << dp(n) << '\n';
+
+    return 0;
+}
+
