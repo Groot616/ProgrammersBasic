7566fa4ff44d82055791af2a3411f6284aacc74b
Groot616 <97540029+Groot616@users.noreply.github.com>
2025-07-05 18:36:33 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.41 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -3,31 +3,23 @@
 
 using namespace std;
 
-void getNumCnt(int number, vector<int> &weaponCnt)
-{
-    int cnt = 0;
-    if(number == 1)
-        weaponCnt.push_back(1);
-    else if(number == 2)
-        weaponCnt.push_back(2);
-    else
-    {
-        for(int i = 1; i <= number / 2; ++i)
-            if(number % i == 0) ++cnt;
-        weaponCnt.push_back(cnt + 1);
-    }
-}
-
 int solution(int number, int limit, int power) {
     int answer = 0;
-    vector<int> weaponCnt;
     
-    for(int i = 1; i <= number; ++i)
-        getNumCnt(i, weaponCnt);
-    for(auto e : weaponCnt)
+    vector<int> divisorCnt(number + 1);
+    divisorCnt[0] = 0;
+    for(int i = 1; i < divisorCnt.size(); ++i)
     {
-        if(e <= limit) answer += e;
-        else answer += power;
+        for(int j = i; j < divisorCnt.size(); j += i)
+            ++divisorCnt[j];
+    }
+    
+    for(int i = 1; i < divisorCnt.size(); ++i)
+    {
+        if(divisorCnt[i] <= limit)
+            answer += divisorCnt[i];
+        else
+            answer += power;
     }
     
     return answer;
