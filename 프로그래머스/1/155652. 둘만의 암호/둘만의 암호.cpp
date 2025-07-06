5da39f600280ecd168513c24e8762dbe12c05be8
Groot616 <97540029+Groot616@users.noreply.github.com>
2025-07-06 18:33:52 +0900

    [level 1] Title: 둘만의 암호, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include <vector>
-#include <set>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -8,35 +8,27 @@ using namespace std;
 string solution(string s, string skip, int index) {
     string answer = "";
     
-    set<int> skipAlpha;
-    for(const auto e : skip)
-        skipAlpha.insert(e);
+    vector<char> alpha;
+    for(int i = 0; i < 26; ++i)
+        alpha.push_back(97 + i);
+    
+    for(int i = 0; i < skip.length(); ++i)
+        alpha.erase(find(alpha.begin(), alpha.end(), skip[i]));
     
     for(int i = 0; i < s.length(); ++i)
     {
-        vector<char> nextAlpha;
-        for(int j = 1; j <= index; ++j)
+        int idx = -1;
+        for(int j = 0; j < alpha.size(); ++j)
         {
-            if(s[i] + j > 122)
-                nextAlpha.push_back(s[i] + j - 26);
-            else
-                nextAlpha.push_back(s[i] + j);
+            if(s[i] == alpha[j])
+                idx = j;
         }
         
-        for(int j = 0; j < nextAlpha.size(); ++j)
-        {
-            auto it = skipAlpha.find(nextAlpha[j]);
-            if(it != skipAlpha.end())
-            {
-                char tmp = nextAlpha[nextAlpha.size() - 1] + 1;
-                if(nextAlpha[nextAlpha.size() - 1] + 1 > 122)
-                    tmp -= 26;
-                nextAlpha.push_back(tmp);
-            }
-        }
-        s[i] = nextAlpha[nextAlpha.size() - 1];
+        if(idx + index >= alpha.size())
+            answer += alpha[(idx + index) % alpha.size()];
+        else
+            answer += alpha[idx + index];
     }
-    answer = s;
-    
+        
     return answer;
 }
