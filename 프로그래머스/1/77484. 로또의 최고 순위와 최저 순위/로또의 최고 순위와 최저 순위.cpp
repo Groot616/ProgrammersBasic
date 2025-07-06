502f5e796f3fbd51b36eeff9532a00334c9d8fe0
Groot616 <97540029+Groot616@users.noreply.github.com>
2025-07-06 18:07:13 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -1,47 +1,39 @@
 #include <string>
 #include <vector>
-#include <set>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(vector<int> lottos, vector<int> win_nums) {
     vector<int> answer;
     
-    set<int> winNum;
-    set<int> myNum;
-    for(const auto e : win_nums)
-        winNum.insert(e);
-    for(const auto e : lottos)
-        myNum.insert(e);
-    
-    int correct = 0;
-    for(auto it = myNum.begin(); it != myNum.end(); ++it)
-    {
-        if(winNum.find(*it) != winNum.end())
-            ++correct;
-    }
-    
     int zeroCnt = 0;
-    for(auto e : lottos)
+    int correctCnt = 0;
+    for(int i = 0; i < lottos.size(); ++i)
     {
-        if(e == 0)
-            zeroCnt++;
+        if(lottos[i] != 0)
+        {
+            auto it = find(win_nums.begin(), win_nums.end(), lottos[i]);
+            if(it != win_nums.end())
+                ++correctCnt;
+        }
+        else
+            ++zeroCnt;
     }
-    
-    if(zeroCnt == 6)
+    if(zeroCnt == 0 && correctCnt == 0)
+    {
+        answer.push_back(6);
+        answer.push_back(6);
+    }
+    else if(zeroCnt == 6)
     {
         answer.push_back(1);
         answer.push_back(6);
     }
-    else if(correct == 0)
-    {
-        answer.push_back(6);
-        answer.push_back(6);
-    }
     else
     {
-        answer.push_back(lottos.size() + 1 - (correct + zeroCnt));
-        answer.push_back(lottos.size() + 1 - correct);
+        answer.push_back(6 - (correctCnt + zeroCnt) + 1);
+        answer.push_back(6 - correctCnt + 1);
     }
     
     return answer;
