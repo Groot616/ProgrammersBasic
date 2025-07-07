42404fe3c4a2b0cc60e2f0dbce87f0f3ad95c6dc
Groot616 <97540029+Groot616@users.noreply.github.com>
2025-07-07 16:06:49 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 3.89 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/README.md
+++ 프로그래머스/2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
 
 ### 성능 요약
 
-메모리: 3.91 MB, 시간: 0.02 ms
+메모리: 3.89 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 10일 21:44:05
+2025년 07월 07일 16:06:47
 
 ### 문제 설명
 
--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -1,14 +1,35 @@
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+void getDivisor(int n, vector<int>& divisor)
+{
+    int element = sqrt(2 * n);
+    for(int i = 1; i <= element; ++i)
+    {
+        if((2 * n) % i == 0)
+        {
+            divisor.push_back(i);
+            int other = (2 * n) / i;
+            if(other != i)
+                divisor.push_back(other);
+        }
+    }
+}
 
 int solution(int n) {
     int answer = 0;
     
-    for(int i = 1; i <= n; ++i)
+    vector<int> divisor;
+    getDivisor(n, divisor);
+    
+    // a = (2 * n - k * (k - 1)) / (2 * k)에서 a가 1 이상의 정수인 경우만 가능
+    for(int i = 0; i < divisor.size(); ++i)
     {
-        if(n % i == 0 && i % 2 != 0)
+        double a = (2.0 * n - divisor[i] * (divisor[i] - 1)) / (2.0 * divisor[i]);
+        if(fmod(a, 1.0) == 0.0 && a >= 1.0)
             ++answer;
     }
     
