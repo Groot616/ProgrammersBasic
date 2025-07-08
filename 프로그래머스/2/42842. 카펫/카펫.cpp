8afed4c6278cf2a6fc696ee9a581281c928cefb2
Groot616 <97540029+Groot616@users.noreply.github.com>
2025-07-08 16:21:10 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,26 +1,27 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     
-    vector<int> nums;
-    int sum = brown + yellow;
-    for(int i = sqrt(sum); i >= 0; --i)
+    int divisor;
+    int total = brown + yellow;
+    for(int i = sqrt(total); i >= 1; --i)
     {
-        if(sum % i == 0)
+        if(total % i == 0)
         {
-            if((i - 2) * (sum / i - 2) == yellow)
-            {
-                answer.push_back(sum / i);
-                answer.push_back(i);
+            divisor = i;
+            if((divisor - 2) * ((total / divisor) - 2) == yellow)
                 break;
-            }
         }
     }
+    answer.push_back(divisor);
+    answer.push_back(total / divisor);
+    sort(answer.rbegin(), answer.rend());
     
     return answer;
 }
