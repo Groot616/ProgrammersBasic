c1e67d949a51e753e5760333b2a0f0201a5c6631
Groot616 <97540029+Groot616@users.noreply.github.com>
2025-07-08 15:35:54 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -3,30 +3,15 @@
 
 using namespace std;
 
-vector<long long> fibo;
-
-long long fibonacci(long long n)
-{
-    if(n == 0 || n == 1)
-        return 1;
-    else
-    {
-        if(fibo[n] != 0)
-            return fibo[n] % 1234567;
-        else
-        {
-            fibo[n] = (fibonacci(n - 2) + fibonacci(n - 1)) % 1234567;
-            return fibo[n];
-        }
-    }
-}
-
 int solution(int n) {
     int answer = 0;
     
-    fibo.resize(n);
-    fibo[0] = fibo[1] = 1;
-    answer = fibonacci(n - 1) % 1234567;
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    
+    for(int i = 2; i <= n; ++i)
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    answer = dp[n];
     
     return answer;
 }
